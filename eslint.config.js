3c82751c4f0975ef4b4a466a46654d53fe73460c
Zemiro014 <zemiro014@gmail.com>
2025-10-12 21:08:35 -0300

    set up eslint, prettier and husky

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+// eslint.config.js
+import js from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  prettierConfig, // desativa regras que conflitam com o Prettier
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        node: true,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      "prettier/prettier": ["error", { singleQuote: true }],
+    },
+  },
+];
