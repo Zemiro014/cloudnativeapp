9584c142cdbd40c64325612a226cd45bdd122def
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-09 19:35:08 -0300

    frontend project created

---
+++ frontend/src/app/page.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cloud Native App</title>
+        <meta name="description" content="A simple Next.js application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p />
+        <h1>Welcome Cloud Native App!</h1>
+      </main>
+    </div>
+  );
+}
