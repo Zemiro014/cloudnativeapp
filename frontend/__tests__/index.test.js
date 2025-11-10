4d0e94b255e5ef4351e83ff50bf15a7ab37b7bc9
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-09 22:17:46 -0300

    test with jest

---
+++ frontend/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom';
+import Home from '@/app/page';
+import { render, screen } from '@testing-library/react';
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /Welcome Cloud Native App!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
---
+++ frontend/jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './src/',
+});
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!**/_app.js',
+    '!**/_document.js',
+    '!**/*.d.ts',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
