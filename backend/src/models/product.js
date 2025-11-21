71f197e599bcaa3c248181005a34181e92b40795
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-21 17:28:57 -0300

    set up model sequelize

---
+++ backend/src/models/product.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.NUMBER,
+    category: DataTypes.STRING,
+    count: DataTypes.INTEGER,
+    rating: DataTypes.NUMBER
+  }, {
+    sequelize,
+    modelName: 'Product',
+    freezeTableName: true,
+  });
+  return Product;
+};
