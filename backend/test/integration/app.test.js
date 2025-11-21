32fcd41c8920456ab400403eaaa490b064492303
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-21 11:48:21 -0300

    Making adjustments for the firest test work

--- backend/test/integration/app.test.js
+++ backend/test/integration/app.test.js
@@ -2,7 +2,7 @@ const supertest = require('supertest');
 const app = require('../../src/app');
 const request = supertest(app);
 
-Test("App test", async () => {
+test("App test", async () => {
   const response = await request.get('/');
   expect(response.status).toBe(200);
   expect(response.text).toBe("Welcome to the backend module!");
