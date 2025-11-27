9107c61fb34d37852675d21884b18a00e6bcb320
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-26 23:07:16 -0300

    Adjustments on pipeline

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,7 @@
-
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
-    experimental: {
-    turbo: {
-      rootDir: __dirname,
-    },
-  },
   env: {
     API_HOST: process.env.API_HOST,
     API_PORT: process.env.API_PORT,
