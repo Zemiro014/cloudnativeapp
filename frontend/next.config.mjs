a7932ae9c525220d873309a9b393119ace28a6ec
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-26 22:39:40 -0300

    Adjustments on next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+    experimental: {
+    turbo: {
+      rootDir: __dirname,
+    },
+  },
   env: {
     API_HOST: process.env.API_HOST,
     API_PORT: process.env.API_PORT,
