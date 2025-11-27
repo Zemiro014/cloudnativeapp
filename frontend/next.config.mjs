e1a3fff2480d96eed6a921af823d03d9c07bc33d
Jeronimo Kulandissa <jeronimo.cossengue.kulandissa@sap.com>
2025-11-26 22:49:48 -0300

    Adjustments on next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,10 @@
+
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
