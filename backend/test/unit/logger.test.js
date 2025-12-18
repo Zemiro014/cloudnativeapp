d964d22f6257f316ea85ccc6b5005675d4b65478
Zemiro014 <zemiro014@gmail.com>
2025-12-17 21:18:36 -0300

    ci: force push images to new eks registry

--- backend/test/unit/logger.test.js
+++ backend/test/unit/logger.test.js
@@ -4,6 +4,7 @@ const logger = require('../../src/utils/logger');
 test('Test logger call', () => {
   logger.info('This is an info message');
   logger.info('This is a log info level');
+
   logger.warning('This is a log warning level');
 
   logger.error('This is a log error level');
